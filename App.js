1340ff7b4dd39be7c6f9cd3f72c744a5be942798
Ranayke Boni Ferreira <ranayke2009@gmail.com>
2023-03-21 20:40:10 -0300

    Started first page and added buttons

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { StatusBar } from "expo-status-bar";
+import Landing from "./components/auth/Landing";
 
-const Stack = createStackNavigator
+const Stack = createStackNavigator;
 
 const App = () => (
   <NavigationContainer>
-
+    <Landing />
   </NavigationContainer>
 );
 
-
 export default App;
-
