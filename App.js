d04ad861b9a2760a84594fc9905a23e35109d2b1
Ranayke Boni Ferreira <ranayke2009@gmail.com>
2023-04-01 09:12:16 -0300

    feat(login): Created Login component

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Landing } from "./components/auth/Landing";
 import { Register } from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,8 +12,17 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Landing} options={{ headerShown: false }}  />
-        <Stack.Screen name="Register" component={Register} options={{ headerShown: false }}  />
+        <Stack.Screen
+          name="Home"
+          component={Landing}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
   );
