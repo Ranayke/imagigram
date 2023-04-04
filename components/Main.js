cdffb8dc4fd84f6b5ae2bbb9bbd5fc1ede07602f
Ranayke Boni Ferreira <ranayke2009@gmail.com>
2023-04-04 16:53:21 -0300

    feat(main): Implemented initial main component

--- components/Main.js
+++ components/Main.js
@@ -7,18 +7,24 @@ import { bindActionCreators } from "redux";
 import { fetchUser } from "../redux/actions";
 
 const Main = (props) => {
+  const { currentUser } = props;
+
   useEffect(() => {
     props.fetchUser();
   }, []);
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Bem vindo!</Text>
+      <Text>Bem vindo {props.currentUser?.name}!</Text>
     </View>
   );
 };
 
+const mapStateToProps = (store) => ({
+  currentUser: store.userState.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) =>
   bindActionCreators({ fetchUser }, dispatch);
 
-export default connect(null, mapDispatchToProps)(Main);
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
